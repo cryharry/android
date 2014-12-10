2cf9b269e8713b4a44a1a7baab5b2de203c90eb1
cryharry <cryharryk@gmail.com>
2014-12-10 17:03:45 +0900

    hellotest7

--- src/com/example/hellotest7/MainActivity.java
+++ src/com/example/hellotest7/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.hellotest7;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
