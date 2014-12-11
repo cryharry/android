1013de5a54f83a3b0a5d30b9629ba7d987d5d450
cryharry <cryharryk@gmail.com>
2014-12-11 17:50:34 +0900

    hellotest12

---
+++ src/com/example/hellotest12/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.hellotest12;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void normal(View v) {
+		NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+		builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_input_add));
+		builder.setSmallIcon(android.R.drawable.ic_input_delete);
+		builder.setContentTitle("내용타이틀");
+		builder.setContentText("내용입니다.");
+		manager.notify(1, builder.build());
+	}
+}
